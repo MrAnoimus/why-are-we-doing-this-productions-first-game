06a39ce30c123dae8fd222c4b5a215ca1fc3de1e
JustEsson@gmail.com <JustEsson@gmail.com@41266af6-17a8-69b9-63a3-9d562b61e693>
2015-01-27 09:14:06 +0000

    Updated, now have background moving in main menu. :D

--- Assignment 1/src/com/GDT/sidm_2014/MenuPage.java
+++ Assignment 1/src/com/GDT/sidm_2014/MenuPage.java
@@ -7,12 +7,13 @@ import android.view.WindowManager;
 
 public class MenuPage extends Activity{
 
+	public static Activity activityTracker;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 				
 		requestWindowFeature(Window.FEATURE_NO_TITLE);// hide title
 	    getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN); //hide top bar
-		setContentView(new MainMenu(this));
+		setContentView(new MainMenu(this, this));
 	}
 }
